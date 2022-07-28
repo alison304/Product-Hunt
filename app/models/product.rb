47643edb4757a50ae404bacf1ab0db118194b99a
Alison Janise Paico Quispe <alison.jpq@gmail.com>
2022-07-28 01:57:28 -0500

    enlace categ

--- app/models/product.rb
+++ app/models/product.rb
@@ -23,5 +23,5 @@ class Product < ApplicationRecord
     has_one_attached :image, :dependent => :destroy
     #uniqueness, length, presence, format con regex
     has_many :product_categories
-    
+    has_many :categories, through: :product_categories  #join
 end
